c98c4707e556df54af31b1e6c233811f0dbff117
abhinav kumar <abhinav.mg.k@gmail.com>
2025-07-14 16:17:08 +0530

    updates done

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -16,6 +22,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Custom manager for published posts.
     
     class Meta:
         ordering = ('-publish',)
